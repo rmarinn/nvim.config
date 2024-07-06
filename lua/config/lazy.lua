d85b59198a4eedae62c94861ebefdc2ed1ad4a34
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-07-06 18:57:49 +0800

    added a lua/dev folder for custom plugins only found in the user's file system

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
 	spec = {
 		-- import your plugins
 		{ import = "plugins" },
+		{ import = "dev" },
 	},
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
