18371f044e490baa74b11f3f8ee70bc993079609
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-07-09 06:07:22 +0800

    added autocommands to run lua, python, and rust while in the editor

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,27 +18,35 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	end,
 })
 
-
---[[
 -- python mappings
 vim.api.nvim_create_autocmd("FileType", {
 	desc = "python mappings",
 	group = vim.api.nvim_create_augroup("python_mappings", { clear = true }),
 	pattern = "python",
 	callback = function(_)
-		vim.keymap.set("n", "<F4>", ":w <bar> exec '!python '.shellescape('%')<CR>")
+		vim.keymap.set("n", "<F5>", ":w <bar> exec '!python '.shellescape('%')<CR>") -- save then run current python file
 	end,
 })
 
 -- lua mappings
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd("filetype", {
 	desc = "lua mappings",
 	group = vim.api.nvim_create_augroup("lua_mappings", { clear = true }),
 	pattern = "lua",
 	callback = function(_)
-		vim.keymap.set("n", "<F4>", function()
-			vim.cmd("source %")
+		vim.keymap.set("n", "<F5>", function()
+			vim.cmd("source %") -- run current lua file
 		end)
 	end,
 })
---]]
+
+-- rust mappings
+vim.api.nvim_create_autocmd("filetype", {
+	desc = "rust mappings",
+	group = vim.api.nvim_create_augroup("rust_mappings", { clear = true }),
+	pattern = "rust",
+	callback = function(_)
+		vim.keymap.set("n", "<F4>", ":w <bar> exec '!cargo check'<CR>") -- save then check if current project can compile
+		vim.keymap.set("n", "<F5>", ":w <bar> exec '!cargo run'<CR>") -- save then compile current project
+	end,
+})
