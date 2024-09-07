0da3af100d108c7cc6b9b41e1a9b1b452368cfa8
rmarinn <34529290+rmarinn@users.noreply.github.com>
2024-09-07 11:07:41 +0800

    enable clipboard in WSL

--- init.lua
+++ init.lua
@@ -7,3 +7,17 @@ require("config.keymaps")
 require("config.options")
 require("config.autocmds")
 require("config.lazy")
+
+-- enabling clipboard on WSL
+vim.g.clipboard = {
+	name = "WslClipboard",
+	copy = {
+		["+"] = "clip.exe",
+		["*"] = "clip.exe",
+	},
+	paste = {
+		["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+	},
+	cache_enabled = 0,
+}
