782b2c889c1d904abc1576d14dc8fe4e3ecc2d78
rmarinn <34529290+rmarinn@users.noreply.github.com>
2025-06-02 08:23:41 +0800

    add dap-go

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -5,13 +5,12 @@ return {
 		"mfussenegger/nvim-dap",
 		"nvim-neotest/nvim-nio",
 		"folke/lazydev.nvim",
+		"https://github.com/leoluz/nvim-dap-go",
 	},
 	config = function()
 		require("lazydev").setup({
 			library = { "nvim-dap-ui" },
 		})
-
-		require("dapui").setup()
 
 		local dap, dapui = require("dap"), require("dapui")
 		dap.listeners.before.attach.dapui_config = function()
@@ -27,19 +26,10 @@ return {
 			dapui.close()
 		end
 
-		vim.keymap.set("n", "<leader>dt", ":DapToggleBreakpoint<CR>", { desc = "Dap Toggle Breakpoint" })
-		vim.keymap.set("n", "<leader>dx", ":DapTerminate<CR>", { desc = "Dap Terminate" })
-		vim.keymap.set("n", "<leader>dl", ":DapStepOver<CR>", { desc = "Dap Step Over" })
-		vim.keymap.set("n", "<leader>dk", ":DapStepInto<CR>", { desc = "Dap Step Into" })
-		vim.keymap.set("n", "<leader>dh", ":DapStepOut<CR>", { desc = "Dap Step Out" })
-		vim.keymap.set("n", "<leader>dc", ":DapContinue<CR>", { desc = "Dap Start or Continue" })
-
-		local dap = require("dap")
 		dap.adapters.codelldb = {
 			type = "server",
 			port = "${port}",
 			executable = {
-				-- if installed via Mason
 				command = vim.fn.stdpath("data") .. "/mason/bin/codelldb.cmd",
 				args = { "--port", "${port}" },
 			},
@@ -47,16 +37,60 @@ return {
 
 		dap.configurations.rust = {
 			{
-				name = "Debug executable",
+				name = "Debug Rust executable",
 				type = "codelldb",
 				request = "launch",
 				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+					local workspace_folder = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+					return vim.fn.input(
+						"Path to executable: ",
+						vim.fn.getcwd() .. "/target/debug/" .. workspace_folder .. ".exe",
+						"file"
+					)
 				end,
 				cwd = "${workspaceFolder}",
 				stopOnEntry = false,
 				args = {},
 			},
 		}
+
+		require("dap-go").setup({
+			dap_configurations = {
+				{
+					type = "go",
+					name = "Attach remote",
+					mode = "remote",
+					request = "attach",
+				},
+			},
+			delve = {
+				path = "dlv.cmd",
+				initialize_timeout_sec = 20,
+				port = "${port}",
+				args = {},
+				build_flags = {},
+				detached = vim.fn.has("win32") == 0,
+				cwd = nil,
+			},
+			tests = {
+				verbose = false,
+			},
+		})
+
+		vim.keymap.set(
+			"n",
+			"<leader>dt",
+			":lua require'dap'.toggle_breakpoint()<CR>",
+			{ desc = "DAP Toggle Breakpoint" }
+		)
+		vim.keymap.set("n", "<leader>dx", ":lua require'dap'.terminate()<CR>", { desc = "DAP Terminate" })
+		vim.keymap.set("n", "<leader>dl", ":lua require'dap'.step_over()<CR>", { desc = "DAP Step Over" })
+		vim.keymap.set("n", "<leader>dk", ":lua require'dap'.step_into()<CR>", { desc = "DAP Step Into" })
+		vim.keymap.set("n", "<leader>dj", ":lua require'dap'.step_out()<CR>", { desc = "DAP Step Out" })
+		vim.keymap.set("n", "<leader>dc", ":lua require'dap'.continue()<CR>", { desc = "DAP Start or Continue" })
+		vim.keymap.set("n", "<leader><leader>dc", ":lua require'dap'.run_last()<CR>", { desc = "DAP Run last" })
+		vim.keymap.set("n", "<leader><leader>dr;", ":lua require'dap'.restart()<CR>", { desc = "DAP Restart" })
+
+		dapui.setup()
 	end,
 }
