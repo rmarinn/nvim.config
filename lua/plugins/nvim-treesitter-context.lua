b1232a2a1a8d41bdc8e57c50200594b6d74a4fc3
rmarinn <34529290+rmarinn@users.noreply.github.com>
2025-11-16 10:51:09 +0800

    add toggle command to treesitter-context

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -4,5 +4,8 @@ return {
 		vim.keymap.set("n", "[c", function()
 			require("treesitter-context").go_to_context(vim.v.count1)
 		end, { silent = true, desc = "Jump to the start of the function" })
+		vim.keymap.set("n", "]c", function()
+			require("treesitter-context").toggle()
+		end, { silent = true, desc = "Toggle treesitter context" })
 	end,
 }
